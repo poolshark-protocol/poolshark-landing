c83b24cb3d5bd6c6eeb8cc73abd0689210923fac
lucasregueiroo <hello@lucasregueiro.com>
2023-03-29 00:23:09 +0200

    Fixed font issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <div className="bg-black font-dmsans bg-[url('/images/bg.svg')] bg-cover overflow-x-hidden">
+    <div className="bg-black font-Satoshi bg-[url('/images/bg.svg')] bg-cover overflow-x-hidden">
       <div className="container mx-auto md:px-0 px-5 flex justify-between items-center">
         <div className="flex items-center gap-x-5 py-6">
           <img className="w-44 mr-10" src="/images/poolsharkmain.png" />
@@ -20,13 +20,13 @@ export default function Home() {
       </div>
       <div className="container md:px-0 px-5 mx-auto md:flex mx-auto space-y-5 md:space-y-0 md:justify-between md:items-center py-20">
         <div className="text-white ">
-          <h1 className="md:text-5xl text-4xl font-bold max-w-lg md:min-w-[500px] leading-tight">Directional Liquidity for Ethereum</h1>
+          <h1 className="md:text-6xl text-4xl font-bold max-w-lg md:min-w-[600px] leading-tight">Directional Liquidity for Ethereum</h1>
           <p className="text-[#615F68] md:max-w-lg mt-5">Poolshark is a directional AMM where users can profit from volatility with less maintenance. Traders can now be LPs too.</p>
         </div>
         <img alt="Poolshark UI elements" className="md:max-w-4xl" src="/images/img1.png" />
       </div>
       <div className="md:px-0 px-5">
-        <h1 className="text-white md:text-5xl text-4xl text-center font-bold md:max-w-lg mx-auto leading-tight mt-20">Directionally balance your LP Position</h1>
+        <h1 className="text-white md:text-6xl text-4xl text-center font-bold md:max-w-2xl mx-auto leading-tight mt-20">Directionally balance your LP Position</h1>
         <h2 className="text-[#615F68] text-center md:max-w-2xl mx-auto mt-5  md:text-base text-sm">Protect against loss from volatility by buying the in-demand side of the liquidity pool. Average into your asset of choice to catch moves in the market.</h2>
         <img alt="ETH and DAI Balance" className="md:max-w-6xl mx-auto" src="/images/img2.png" />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'dmsans': ['DM Sans', 'sans-serif']
+        'Satoshi': ['Satoshi']
       },
     },
   },
