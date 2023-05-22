8a986e11ad49ef4272fee939d92a8182aa2b9e28
lucasregueiroo <hello@lucasregueiro.com>
2023-05-22 11:08:54 +0100

    twitter link fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="container mx-auto md:px-0 px-5 flex justify-between items-center">
         <div className="flex items-center gap-x-5 py-6">
           <img className="w-44 mr-10" src="/images/poolsharkmain.png" />
-          <a href="https://twitter.com/poolsharkslabs" target="_blank" className="hover:opacity-80 transition-all">
+          <a href="https://twitter.com/poolsharklabs" target="_blank" className="hover:opacity-80 transition-all">
           <svg fill="currentColor" className="text-white w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path fill-rule="evenodd" d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" /></svg>
           </a>
           <a href="https://discord.com/invite/HJhrD9sZ6K" target="_blank" className="hover:opacity-80 transition-all">
@@ -93,7 +93,7 @@ export default function Home() {
           <a href="https://poolshark-protocol.medium.com/" target="_blank" className="flex items-center bg-[#bdf] px-9 py-3 rounded-xl text-sm w-min text-black gap-x-4 hover:opacity-80 transition-all">
             <svg fill="currentColor" className="w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Medium</title><path d="M13.54 12a6.8 6.8 0 01-6.77 6.82A6.8 6.8 0 010 12a6.8 6.8 0 016.77-6.82A6.8 6.8 0 0113.54 12zM20.96 12c0 3.54-1.51 6.42-3.38 6.42-1.87 0-3.39-2.88-3.39-6.42s1.52-6.42 3.39-6.42 3.38 2.88 3.38 6.42M24 12c0 3.17-.53 5.75-1.19 5.75-.66 0-1.19-2.58-1.19-5.75s.53-5.75 1.19-5.75C23.47 6.25 24 8.83 24 12z"/></svg>
             Medium</a>
-            <a href="https://twitter.com/poolsharkslabs" target="_blank" className="flex items-center bg-[#bdf] px-9 py-3 rounded-xl text-sm w-min text-black gap-x-4 hover:opacity-80 transition-all">
+            <a href="https://twitter.com/poolsharklabs" target="_blank" className="flex items-center bg-[#bdf] px-9 py-3 rounded-xl text-sm w-min text-black gap-x-4 hover:opacity-80 transition-all">
             <svg fill="currentColor" className=" w-5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path fill-rule="evenodd" d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" /></svg>
             Twitter</a>
             <a href="https://discord.com/invite/HJhrD9sZ6K" target="_blank" className="flex items-center bg-[#bdf] px-9 py-3 rounded-xl text-sm w-min text-black gap-x-4 hover:opacity-80 transition-all">
