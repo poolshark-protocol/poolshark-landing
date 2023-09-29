f03f7fef8839040aa4cbfba6d167159c66ad1714
lucasregueiroo <hello@lucasregueiro.com>
2023-09-29 18:42:02 +0800

    Finish landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,21 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'Jetbrains': ['"JetBrains Mono"', 'monospace'],
+      'Helvetica': ['Helvetica Neue', 'sans-serif']
+    },
     extend: {
-      fontFamily: {
-        'Satoshi': ['Satoshi']
+      colors: {
+        main: "#022F88",
+        main1: "#000A2C",
+        main2: "#227BED",
+        background: "#06172D",
+        grey: "#2C2E33",
+        grey1: "#646364",
+        grey2: '#494949',
+        grey3: "#5F5F5F",
+        dark: "#0C0C0C",
       },
     },
   },
