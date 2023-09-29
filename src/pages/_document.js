aca438ad3c3b1a7eb84478359ea0512e90ea0600
lucasregueiroo <hello@lucasregueiro.com>
2023-09-29 21:31:18 +0800

    fix meta images

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,13 +5,13 @@ export default function Document() {
     <Html lang="en">
      <Head>
         <title>Poolshark</title>
-        <meta name="description" content="Poolshark is a directional AMM where users can profit from volatility with less maintenance. Traders can now be LPs too."/>
+        <meta name="description" content="Poolshark is a cutting-edge AMM where users can create directional positions to profit from volatility. Traders are able to utilise their orders to provide liquidity."/>
         <meta property="og:title" content="Poolshark" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="https://poolshark.fi/" />
 <meta property="og:image" content="/images/og.png" />
 <meta name="twitter:title" content="Poolshark"/>
-<meta name="twitter:description" content="Poolshark is a directional AMM where users can profit from volatility with less maintenance. Traders can now be LPs too."/>
+<meta name="twitter:description" content="Poolshark is a cutting-edge AMM where users can create directional positions to profit from volatility. Traders are able to utilise their orders to provide liquidity."/>
 <meta name="twitter:image" content="/images/og.png"/>
 <meta name="twitter:card" content="summary_large_image"/>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
